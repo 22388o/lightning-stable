fb6c3879b6389099f6744c6823f8393cc2424611
leffw <93302536+leffw@users.noreply.github.com>
2022-12-10 22:20:27 -0300

    feat: add table db (balance, transaction)

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from configs import PATH
-from peewee import SqliteDatabase, Model, TextField, FloatField, ForeignKeyField
+from peewee import SqliteDatabase, Model, DateTimeField, TextField, FloatField
 
 database = SqliteDatabase(f"{PATH}/data/database.db")
 
@@ -12,8 +13,22 @@ class User(BaseModel):
     password = TextField()
 
 class Balance(BaseModel):
-    username = TextField()
-    currency = TextField(choices=["BTC", "USD"])
-    balance = FloatField(default=0)
+    username   = TextField()
+    currency   = TextField(choices=["BTC", "USD"])
+    balance    = FloatField(default=0)
+    created_at = DateTimeField(default=datetime.now)
+    updated_at = DateTimeField(default=datetime.now)
 
-database.create_tables([User, Balance])
+class Transaction(BaseModel):
+    txid         = TextField()
+    username     = TextField()
+    destination  = TextField()
+    currency     = TextField(choices=["BTC", "USD"])
+    value        = FloatField(default=0)
+    status       = TextField(choices=["settled", "pending", "canceled"])
+    typeof       = TextField(column_name="type", choices=["withdraw", "deposit"])
+    description  = TextField(null=True)
+    created_at   = DateTimeField(default=datetime.now)
+    updated_at   = DateTimeField(default=datetime.now)
+
+database.create_tables([User, Balance, Transaction])
